b1d1c01b119dbc14b81cbd2bb1a6a30b224b97f8
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-02-26 14:44:16 -0500

    adding folders

---
+++ questions.js
@@ -0,0 +1,25 @@
+var quiz = [{
+  title: 'Inside which HTML do we put the           Javascript?',
+  options: ['<scripting>', '<js>', '<javascript>', '<script>'],
+  answer: 2 
+},
+{
+ title: 'Where is the correct place to put Javascript?',
+ options: ['Both the <head> section and the <body> section are correct', 'The <head> section', 'The <body> section'],
+ answer: 'The <body> section',
+},
+{
+ title: 'The external JavaScript file must contain the <script> tag.',
+ options: ['False', 'True'],
+ answer: 'False',
+},
+{
+ title: 'How can you add a comment in Javascript?',
+ options: ['<!--This is a comment-->', '//This is a comment', 'This is a comment'],
+ answer: '//This is a comment'
+},
+{
+ title: 'Javascript is the same as Java.',
+ options: ['True', 'False'],
+ answer: 'False',
+}];
